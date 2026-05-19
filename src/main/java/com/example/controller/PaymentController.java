6c9c17f179b758b51dee1ab6b5658617117ae3d4
Julianxa <julianip.hk@gmail.com>
2026-05-19 16:54:58 +0800

    refactor

--- src/main/java/com/example/controller/PaymentController.java
+++ src/main/java/com/example/controller/PaymentController.java
@@ -2,9 +2,14 @@ package com.example.controller;
 
 import com.example.model.dto.*;
 import com.example.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +25,26 @@ import java.time.LocalDateTime;
 public class PaymentController {
     private final PaymentService paymentService;
 
+    @Operation(
+            summary = "Retrieve payment details by session ID",
+            description = "Retrieve payment details by session ID after payment.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Retrieve payment details successfully",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = GetPaymentDetailsResponseDTO.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid request data",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponseDTO.class))),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+            }
+    )
     @PostMapping("/payments/details")
     public ResponseEntity<?> getPaymentDetails(
             @RequestBody GetPaymentDetailsRequestDTO request) {
@@ -33,6 +58,26 @@ public class PaymentController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "Refund a payment online/offline",
+            description = "Online full refund or Offline partial refund.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Refund the payment successfully",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = RefundResponseDTO.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid request data",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorResponseDTO.class))),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+            }
+    )
     @PostMapping("/payments/refund")
     public ResponseEntity<?> refund(
             @RequestBody RefundRequestDTO request) {
