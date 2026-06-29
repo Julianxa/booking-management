f9aef2795b7d880825325c275db766e004b1673d
Julianxa <julianip.hk@gmail.com>
2026-06-29 12:19:14 +0800

    email type is required in ResendBookingEmailRequestDTO

--- src/main/java/com/example/model/dto/ResendBookingEmailRequestDTO.java
+++ src/main/java/com/example/model/dto/ResendBookingEmailRequestDTO.java
@@ -3,6 +3,7 @@ package com.example.model.dto;
 import com.example.constant.Enums;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,8 @@ import lombok.NoArgsConstructor;
 public class ResendBookingEmailRequestDTO {
     @JsonProperty("email_template_id")
     private String emailTemplateId;
+
+    @NotNull(message = "email_type is required")
     @JsonProperty("email_type")
     private Enums.BookingEmailType emailType;
 }
